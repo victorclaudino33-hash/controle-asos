58d55c937e014c8a477121b8993f81ee13275a54
empresaaonline33-dot <empresaaonline33@gmail.com>
2026-08-24 22:00:50 -0400

    Revert "Reorganiza em painel/ e robozinho/, adiciona API e libs do robô"
    This reverts commit 7071162aedb6cc8cd1049abdfa3d939c68a4e8ca.

--- robozinho/lib/run-local.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { runSync } from './run-sync.js';
-
-runSync()
-  .then(r => { console.log('Resultado:', r); process.exit(0); })
-  .catch(e => { console.error('Erro:', e); process.exit(1); });
